bbb4e06cefc7df37fc3e05e11a5ad57a3316d668
Kevin Tan <tankevin7600@gmail.com>
2018-11-14 21:47:44 -0800

    just finished sliding animation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import {ServicesService} from './services.service';
+import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[ServicesService]
+  providers:[ServicesService],
+  animations: [
+    trigger('resultsAnimation', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)'}),
+        animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({transform: 'translateX(-100%)'}))
+      ])
+    ]),
+    trigger('DetailsAnimation',[
+      transition(':enter', [
+        style({transform: 'translateX(100%)'}),
+        animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
+  visible=false;
   constructor(public service:ServicesService){}
 }
